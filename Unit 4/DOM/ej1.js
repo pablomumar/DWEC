705aea14aff5bba75a310b917c66c65c4fdc3894
CheiZz <pablomumar@hotmail.com>
2017-11-03 12:34:37 +0100

    Fixing issues with event

---
+++ Unit 4/DOM/ej1.js
@@ -0,0 +1,35 @@
+var setPage = () => {
+    for (x=0; x<3; x++){
+        let newp = document.createElement('p');
+        newp.innerHTML = '<a href="https://www.google.es/">ENLACE#'+(x+1)+'</a><br><a href="https://www.google.es/">ENLACE2#'+(x+1)+'</a><br>';
+        document.body.appendChild(newp);
+    }
+    let button = document.createElement('input');
+    document.body.appendChild(button);
+    button.type = 'submit';
+    button.value = 'Info';
+    button.onclick = 'darInfo';
+}
+
+//Nota: No entra en darInfo() :(
+
+var darInfo = () => {
+    let newp = document.createElement('p');
+    let direc = document.getElementsByTagName('p');
+    newp.innerHTML = 'Nº de enlaces: ' + direc.length;
+    newp.innerHTML = '.<br> Direccion del penúltimo enlace: ' + direc[1].href;
+    let n_link = comprobar_link(direc);
+    newp.innerHTML = '.<br> Nº de enlaces a Google: ' + n_link;
+    let n_link_3p = comprobar_link(direc[2]);
+    newp.innerHTML = '.<br> Nº de enlaces del 3º párrafo: ' + n_link;
+}
+
+function comprobar_link(direc){
+    let n = 0;
+    for (let link in direc) {
+        if (link.href === "https://www.google.es/") {
+          n++
+        }
+    }
+    return n
+}
