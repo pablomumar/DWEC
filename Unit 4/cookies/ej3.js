86333cdc105406b76324204898b8458941721479
CheiZz <pablomumar@hotmail.com>
2017-11-03 08:45:45 +0100

    Unload erase localhost cookies

---
+++ Unit 4/cookies/ej3.js
@@ -0,0 +1,29 @@
+function setCookie(){
+    usuario = document.getElementById('UserName');
+    document.cookie ? (
+        cookiesplit = document.cookie.split(' '),
+            alert('Hola ' + cookiesplit[0]),
+            document.write("<input type='submit' onclick='borrarcookies()' value='Borrar'>")
+    ):(
+        document.write("UserName:&nbsp<input type='text' id='UserName'/>"),
+            document.write("<br><input type='submit' onclick='guardarUsuario()' value='Guardar'>"),
+            document.write("<input type='submit' onclick='borrarcookies()' value='Borrar'>"));
+}
+
+function guardarUsuario() {
+    tiempoExpire = new Date();
+    tiempoExpire.setSeconds(5000);
+    tiempoExpireString = tiempoExpire.toUTCString();
+    nombreUsuario = document.getElementById('UserName').value;
+    document.cookie = 'username=' + nombreUsuario +'; expires=' + tiempoExpireString ;
+    alert('Hola ' + nombreUsuario + ' Expires: ' + tiempoExpireString);
+    console.log(document.cookie);
+}
+
+function borrarcookies() {
+    document.cookie = 'username=; expires="Sun, 15 Jul 2012 00:00:01 GMT"';
+}
+
+addEventListener('unload', borrarcookies);
+
+//Este evento realiza la función borrar cookies al cerrar el navegador
