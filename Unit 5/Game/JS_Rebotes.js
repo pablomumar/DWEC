acd03511c9faefc64d9f020d1f9652a5d43e4b8e
CheiZz <pablomumar@hotmail.com>
2017-11-09 12:14:10 +0100

    Simple bounce (just one ball)

---
+++ Unit 5/Game/JS_Rebotes.js
@@ -0,0 +1,34 @@
+var bola = document.getElementById('ball');
+var arriba = false;
+var derecha = true;
+var movimiento_bola = [2, 2];
+var FPS = 60;
+
+var update_coord = () =>{
+    if (parseInt(bola.getAttribute('cx')) > 795){
+        derecha = false;
+        movimiento_bola = [movimiento_bola[0] * -1, movimiento_bola[1]]
+    } else if (parseInt(bola.getAttribute('cx')) < 5){
+        derecha = true;
+        movimiento_bola = [movimiento_bola[0] * -1, movimiento_bola[1]]
+    }  else if (parseInt(bola.getAttribute('cy')) > 595){
+        arriba = true;
+        movimiento_bola = [movimiento_bola[0], movimiento_bola[1] * -1]
+    } else if (parseInt(bola.getAttribute('cy')) < 5){
+        arriba = false;
+        movimiento_bola = [movimiento_bola[0], movimiento_bola[1] * -1]
+    }
+}
+
+
+var move = () => {
+    update_coord();
+    console.log(parseInt(bola.getAttribute('cy')));
+    bola.setAttribute('cx', parseInt(bola.getAttribute('cx')) + movimiento_bola[0]);
+    bola.setAttribute('cy', parseInt(bola.getAttribute('cy')) + movimiento_bola[1]);
+}
+
+function movimiento() {
+    setInterval(move, 1000 / FPS)
+}
+
