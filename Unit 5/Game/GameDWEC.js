dcfe346f392e0bd587477bd7a287c2d2f6448e32
CheiZz <pablomumar@hotmail.com>
2017-11-16 14:32:01 +0100

    Creacion de piezas, serpiente y spawn de comida

--- Unit 5/Game/GameDWEC.js
+++ Unit 5/Game/GameDWEC.js
@@ -1,3 +1,5 @@
+var svg = "http://www.w3.org/2000/svg";
+
 class Partida{
     constructor(){
 
@@ -14,8 +16,19 @@ class Comida{
         this.y_comida = y;
     }
 
-    spawn(){
+    getPos(){
+        this.x_comida = parseInt(document.getElementById('comida').getAttribute('cx'));
+        this.y_comida = parseInt(document.getElementById('comida').getAttribute('cy'));
+        console.log(this.x_comida, this.y_comida);
+        return [this.x_comida, this.y_comida];
+    }
 
+    spawn(){
+        let posicion = [Math.floor(Math.random() * (750 - 50)) + 50, Math.floor(Math.random() * (550 - 50)) + 50];
+        console.log(posicion);
+        document.getElementById('comida').setAttribute('cx', posicion[0]);
+        document.getElementById('comida').setAttribute('cy', posicion[1]);
+        this.getPos();
     }
 }
 
@@ -24,9 +37,19 @@ class Torso{
         this.x_torso = x;
         this.y_torso = y;
         this.velocidad = velocidad;
+        this.up=false;
+        this.down=false;
+        this.left=false;
+        this.right=true;
     }
 
-    comer(){}
+    showStats(){
+        console.log(this.x_torso, this.y_torso)
+    }
+
+    comer(){
+
+    }
 
     movimiento(){}
 
@@ -36,6 +59,10 @@ class Torso{
 class Serpiente{
     constructor(lista_torsos){
         this.lista_torsos = lista_torsos;
+    }
+
+    anadirListaT(){
+
     }
 
     checkPos(){}
@@ -52,3 +79,34 @@ class HUD{
 
     showPuntuacion(){}
 }
+
+function generaTorso(lista_serp){
+    let canvas = document.getElementById('mySVG');
+    let newTorso = document.createElementNS(svg, 'rect');
+    canvas.appendChild(newTorso);
+    newTorso.id = 'torso' + (lista_serp.length - 1);
+    newTorso.setAttributeNS(null, 'height', 10);
+    newTorso.setAttributeNS(null, 'width', 10);
+    newTorso.setAttributeNS(null, 'fill', 'black');
+    console.log('serpiente antes: ' + lista_serp.length);
+    (lista_serp.length <= 1) ? (
+        newTorso.setAttributeNS(null, 'x', 400),
+        newTorso.setAttributeNS(null, 'y', 300),
+        newTorso = new Torso(400, 300, 2),
+        lista_serp.push(newTorso)
+    ) : (
+        x_t = lista_serp[(lista_serp.length -1)].x_torso,
+        y_t = lista_serp[(lista_serp.length -1)].y_torso,
+        newTorso.setAttributeNS(null, 'x', x_t),
+        newTorso.setAttributeNS(null, 'y', y_t),
+        newTorso = new Torso(lista_serp[(lista_serp.length -1)].x_torso, lista_serp[(lista_serp.length -1)].y_torso, 2),
+        lista_serp.push(newTorso)
+    );
+
+    console.log('esta es la serpiente ahora: ' + lista_serp + newTorso.id)
+}
+
+let Snake = new Serpiente(lista_torsos = []);
+generaTorso(Snake.lista_torsos);
+let comida = new Comida(0,0);
+comida.spawn();
