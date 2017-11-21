857b95cd645f63facb0dc90b94f865be3b8197ad
CheiZz <pablomumar@hotmail.com>
2017-11-21 12:36:06 +0100

    Working on it...

--- Unit 5/MVC/MVC_1.js
+++ Unit 5/MVC/MVC_1.js
@@ -1,29 +1,13 @@
-var regex_id_titulo = /^[a-zA-Z ]+^\d{9}$/;
-var regex_id_texto = 'TexTpostIt' + /^\d{9}$/;
+//Modelo ---------------------------------------------------------------------
 
 class PostIt{
-    constructor(z_titulo, z_texto,t_titulo, t_texto, x, y, marco){
-        this.z_titulo = z_titulo;
-        this.z_texto = z_texto;
+    constructor(t_titulo, t_texto, marco){
         this.t_titulo = t_titulo;
         this.t_texto = t_texto;
-        this.x = x;
-        this.y = y;
         this.marco = marco;
-        // Falta el atributo hora para la hora de creacion
+        this.hora = new Date();
     }
 
-    addText(event){ //evento onclick(por ahora) para hallar en que input estoy escribiendo y añadir value en la clase
-        let id_click = event.target.id;
-        console.log('eee', id_click);
-        if (id_click && id_click.match(regex_id_titulo)){
-            this.t_titulo = document.getElementById(id_click).value;
-            console.log('titulo: ' + this.t_titulo)
-        } else if (id_click && id_click.match(regex_id_texto)){
-            this.t_texto = document.getElementById(id_click).value;
-            console.log('texto: ' + this.t_texto)
-        }
-    }
 }
 
 class ConjuntoPostIt{
@@ -38,7 +22,20 @@ class ConjuntoPostIt{
 
 var mis_postit = new ConjuntoPostIt();
 
-function crear(){
+
+
+
+
+
+// Controlador ----------------------------------------------------------------
+
+class controlador{
+    constructor(controlador){
+        this.controlador = controlador;
+    }
+
+
+    static crear(){
     let z_marco = document.createElement('div');
     let z_titulo = document.createElement('div');
     let z_texto = document.createElement('div');
@@ -49,14 +46,18 @@ function crear(){
     z_marco.appendChild(z_texto);
     z_titulo.appendChild(t_titulo);
     z_texto.appendChild(t_texto);
-    let postit = new PostIt(z_titulo, z_texto, t_titulo, t_texto, 0, 0, z_marco);
+    let postit = new PostIt(t_titulo, t_texto, z_marco);
     mis_postit.add_pi(postit);
     z_marco.id = 'postIt' + (mis_postit.lista_postit.length - 1);
     t_titulo.id = 'TitulopostIt' + (mis_postit.lista_postit.length - 1);
     t_texto.id = 'TexTpostIt' + (mis_postit.lista_postit.length - 1);
     console.log(postit, mis_postit, postit.marco.id)
+    }
 }
 
-crear();
-crear();
-addEventListener('click', mis_postit.lista_postit[0].addText);
+window.onload = () => {
+    controlador = new controlador();
+};
+
+
+document.getElementById('button').addEventListener('click', controlador.crear);
