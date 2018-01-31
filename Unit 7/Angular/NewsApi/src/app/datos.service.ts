9c430bdf38416fce51b28206bc9bdb76b143aaed
CheiZz <pablomumar@hotmail.com>
2018-01-31 12:42:40 +0100

    Improving request

--- Unit 7/Angular/NewsApi/src/app/datos.service.ts
+++ Unit 7/Angular/NewsApi/src/app/datos.service.ts
@@ -5,12 +5,28 @@ import { Observable } from "rxjs/Observable";
 @Injectable()
 export class DatosService {
   entradaKey: string;
+  filtro: string;
+  country: string;
+  category: string;
+  busq_fecha: string;
 
   constructor(private $http:HttpClient) {
   }
 
-  LoadData(entradaKey) :Observable<any>{
-    this.entradaKey = 'q=' + entradaKey;
-    return this.$http.get('https://newsapi.org/v2/everything?q=' + this.entradaKey + '&apiKey=420b00ab4edb4a35bb86d393edd69dda');
+  LoadDataEvery(entradaKey, filtro, country, busq_fecha) :Observable<any>{
+    entradaKey.length == 0 ? this.entradaKey = '' : this.entradaKey = 'q=' + entradaKey + '&';
+    filtro.length == 0 ? this.filtro = '' : this.filtro = 'sortBy=' + filtro + '&';
+    country.length == 0 ? this.country = '' : this.country = 'language=' + country + '&';
+    busq_fecha.length == 0 ? this.busq_fecha = '' : this.busq_fecha = busq_fecha + '&';
+    console.log('Keyb: ' + this.entradaKey);
+    console.log('filt: ' + this.filtro);
+    console.log('lang: ' + this.country);
+    console.log('fecha: ' + this.busq_fecha);
+    return this.$http.get('https://newsapi.org/v2/everything?' + this.entradaKey + this.filtro + this.country + this.busq_fecha +'apiKey=420b00ab4edb4a35bb86d393edd69dda');
+  }
+
+  LoadSourceData(category) :Observable<any>{
+    this.category = 'category=' + category;
+    return this.$http.get('https://newsapi.org/v2/sources?' + this.category + '&apiKey=420b00ab4edb4a35bb86d393edd69dda')
   }
 }
--- Unit 7/Angular/NewsApi/src/app/header/header.component.ts
+++ Unit 7/Angular/NewsApi/src/app/header/header.component.ts
@@ -11,21 +11,118 @@ export class HeaderComponent implements OnInit {
   ListaNoticias: Array<any>[];
 
   keyboard = '';
-  private filtro = false;
-  private idioma = false;
+  filtro = '';
+  country = '';
+  category = '';
+  date = new Date();
+  fecha_act = '';
+  busq_fecha = '';
 
   constructor(private datos: DatosService) {
   }
 
   hacerRequest(){
-    this.datos.LoadData(this.keyboard).subscribe(data => {
+    this.datos.LoadDataEvery(this.keyboard, this.filtro, this.country, this.busq_fecha).subscribe(data => {
       console.log(data);
-     // this.ListaNoticias.push(data.articles);
+     //this.ListaNoticias.push(data);
     })
   }
 
   pasarArrayReq(){
     return this.ListaNoticias;
+  }
+
+  categoryEvery(){
+    return this.category='';
+  };
+
+  categorySport(){
+    this.datos.LoadSourceData('sport').subscribe(data=>
+      console.log(data)
+  )
+  };
+
+  categoryScience(){
+    this.datos.LoadSourceData('science').subscribe(data=>
+      console.log(data)
+  )
+  };
+
+  categoryEntert(){
+    this.datos.LoadSourceData('entertainment').subscribe(data=>
+      console.log(data)
+  )
+  };
+
+  countryEvery(){
+    return this.country='';
+  };
+
+  countryEsp(){
+    return this.country='es';
+  };
+
+  countryUK(){
+    return this.country='gb';
+  };
+
+  countryFR(){
+    return this.country='fr';
+  };
+
+  countryDE(){
+    return this.country='de';
+  };
+
+  obtenerfecha(){
+    this.fecha_act = this.date.getFullYear() + '-' + (this.date.getMonth() + 1) + '-' + this.date.getDate();
+    return this.fecha_act
+  }
+
+  obtenerfechaMenosUnMes(){
+    if (this.date.getMonth() == 0){
+      return (this.date.getFullYear() - 1) + '-' + (this.date.getMonth() + 12) + '-' + this.date.getDate();
+    } else {
+      return this.date.getFullYear() + '-' + (this.date.getMonth() + 1) + '-' + this.date.getDate();
+    }
+  }
+
+  obtenerfechaAyer(){
+    if (this.date.getDate() == 1 && this.date.getMonth() == 0){
+      return (this.date.getFullYear() - 1) + '-' + (this.date.getMonth() + 12) + '-' + (this.date.getDate() +30);
+    } else if (this.date.getDate() == 1 && this.date.getMonth() != 0){
+      return this.date.getFullYear() + '-' + (this.date.getMonth()) + '-' + (this.date.getDate() + 27);
+    } else {
+      return this.date.getFullYear() + '-' + (this.date.getMonth() + 1) + '-' + (this.date.getDate() - 1);
+    }
+  }
+
+  whenAlways(){
+    return this.busq_fecha = '';
+  }
+
+  whenToday(){
+    return this.busq_fecha = 'from=' + this.obtenerfechaAyer();
+  }
+
+  whenYesterday(){
+    return this.busq_fecha = 'from=' + this.obtenerfechaAyer() + '&to=' + this.obtenerfecha();
+  }
+
+  whenMonth(){
+    return this.busq_fecha = 'from=' + this.obtenerfechaMenosUnMes() + '&to=' + this.obtenerfecha();
+  }
+
+  preferencyLast(){
+    return this.filtro='publishAt';
+  }
+
+  preferencyPopular(){
+    return this.filtro='popularity';
+  }
+
+  preferencyRelev(){
+    return this.filtro='relevancy';
   }
 
   ngOnInit() {
